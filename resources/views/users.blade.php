687e4845dd766f597a19441d0ae6db41e426865d
ashraful1910 <ashraful1910@gmail.com>
2024-08-31 15:38:27 +0600

    updated status of users list page

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -64,7 +64,7 @@
           <!-- Messages: style can be found in dropdown.less-->
           <li class="dropdown messages-menu">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-             
+
             </a>
             <ul class="dropdown-menu">
               <li class="header">You have 4 messages</li>
@@ -264,7 +264,7 @@
                 </p>
               </li>
               <!-- Menu Body -->
-       
+
               <!-- Menu Footer-->
               <li class="user-footer">
                 <div class="pull-left">
@@ -371,9 +371,9 @@
                 <td>{{ $data->created_at }}</td>
                 <td>
                     <center>
-                    @if ($data->status === 1)
+                    @if ($data->status == 1)
                         <i class="bi bi-check-circle-fill fa-lg text-success status-icon" title="Active"></i>
-                    @elseif ($data->status === 0)
+                    @elseif ($data->status == 0)
                         <i class="bi bi-x-circle-fill fa-lg text-warning status-icon" title="Inactive"></i>
                     @else
                         <span class="text-muted">Unknown</span>
